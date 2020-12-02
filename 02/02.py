8051cff8e1fea1542e3250c6a0cb3f383f2391fa
Dasara Shullani <shullani.dasara@gmail.com>
2020-12-02 13:05:42 +0100

    problem and code D2.2

--- 02/02.py
+++ 02/02.py
@@ -8,7 +8,15 @@ def count_letter(my_string, letter):
     return count
 
 
-# read file
+def check_position(my_string, letter, min_max):
+    min_value = int(min_max[0]) - 1
+    max_value = int(min_max[-1]) - 1
+    if (my_string[min_value] == letter) ^ (my_string[max_value] == letter):
+        return True
+    return False
+
+
+count_flag = False
 correct_passwords = 0
 wrong_passwords = 0
 with open("02/input.txt", 'r') as f:
@@ -20,12 +28,22 @@ with open("02/input.txt", 'r') as f:
         target_rule = tmp_sep[0].split(' ')
         min_max = target_rule[0].split('-')
         letter = target_rule[-1]
-        cl = count_letter(target_password, letter)
-        if cl >= int(min_max[0]) and cl <= int(min_max[1]):
+        ok_flag = False
+        if count_flag:
+            cl = count_letter(target_password, letter)
+            ok_flag = cl >= int(min_max[0]) and cl <= int(min_max[1])
+        else:
+            ok_flag = check_position(target_password, letter, min_max)
+
+        if ok_flag:
             correct_passwords += 1
         else:
             wrong_passwords += 1
-            #print(target_password, letter, min_max[0], min_max[1])
+
+if count_flag:
+    print('Min-Max frequency')
+else:
+    print('Min-Max indexing')
 
 print("OK:", correct_passwords)
 print("NOP:", wrong_passwords)

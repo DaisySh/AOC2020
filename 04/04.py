fc0069edd07c0fe3b885b2cc0e37678922c8de8a
Dasara Shullani <shullani.dasara@gmail.com>
2020-12-04 20:21:34 +0100

    add code D4.2

--- 04/04.py
+++ 04/04.py
@@ -1,4 +1,6 @@
 #! /usr/bin/env python3
+import string
+
 
 def read_passports(in_file):
     passport_list = []
@@ -24,6 +26,43 @@ def count_pass_newline(in_file):
         return len(elenco)
 
 
+def is_hex_str(s):
+    return set(s).issubset(string.hexdigits)
+
+
+def check_tag_value(tag, value):
+    eye_color = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+    return_value = False
+    if tag == 'byr':
+        if len(value) == 4 and int(value) >= 1920 and int(value) <= 2002:
+            return_value = True
+    if tag == 'iyr':
+        if len(value) == 4 and int(value) >= 2010 and int(value) <= 2020:
+            return_value = True
+    if tag == 'eyr':
+        if len(value) == 4 and int(value) >= 2020 and int(value) <= 2030:
+            return_value = True
+    if tag == 'hgt':
+        cm_in = value[-2:]
+        if cm_in == 'cm' and int(value[:-2]) >= 150 and int(value[:-2]) <= 193:
+            return_value = True
+        elif cm_in == 'in' and int(value[:-2]) >= 59 and int(value[:-2]) <= 76:
+            return_value = True
+        else:
+            return_value = False
+    if tag == 'hcl':
+        if value[0] == '#' and len(value[1:]) == 6 and is_hex_str(value[1:]):
+            return_value = True
+        else:
+            return_value = False
+    if tag == 'ecl':
+        if len(value) == 3 and value in eye_color:
+            return_value = True
+    if tag == 'pid':
+        return_value = len(value) == 9
+    return return_value
+
+
 in_file = "04/input.txt"
 passport_list = read_passports(in_file)
 # tags
@@ -36,7 +75,7 @@ for item in passport_list:
     local_tags = []
     for elem in elem_list:
         elem_tag_value = elem.split(':')
-        if elem_tag_value[0] != ignore_tag:
+        if elem_tag_value[0] != ignore_tag and check_tag_value(elem_tag_value[0], elem_tag_value[1]):
             local_tags.append(elem_tag_value[0])
     local_tags = set(local_tags)
     pass_match_tags = mandatory_tags.intersection(local_tags)

2a0a197bc71acb858fbdd3aa6ed9823ea9b0a4d1
Dasara Shullani <shullani.dasara@gmail.com>
2020-12-08 08:50:06 +0100

    clean code D8.1

--- 08/08.py
+++ 08/08.py
@@ -14,6 +14,29 @@ def get_instruction(i_str):
     return i_instruction, i_simbol, i_number
 
 
+def count_acc(my_boot, access_flag):
+    i = 0
+    acc = 0
+    while i < len(my_boot):
+        i_item = my_boot[i].strip()
+        i_instruction, i_simbol, i_number = get_instruction(i_item)
+        if not access_flag[i]:
+            access_flag[i] = True
+            if i_instruction == 'nop':
+                i += 1
+            elif i_instruction == 'acc':
+                t = "{} {} {}".format(acc, i_simbol, i_number)
+                print(t)
+                acc = eval(t)
+                i += 1
+            elif i_instruction == 'jmp':
+                t = "{} {} {}".format(i, i_simbol, i_number)
+                print(t)
+                i = eval(t)
+        else:
+            return acc
+
+
 # in_file = '08/input2.txt'
 in_file = '08/input.txt'
 
@@ -23,25 +46,7 @@ if len(my_boot[-1].strip()) == 0:
     my_boot = my_boot[:-2]
 
 access_flag = [False for item in my_boot]
-i = 0
-acc = 0
-while i < len(my_boot):
-    i_item = my_boot[i].strip()
-    i_instruction, i_simbol, i_number = get_instruction(i_item)
-    if not access_flag[i]:
-        access_flag[i] = True
-        if i_instruction == 'nop':
-            i += 1
-        elif i_instruction == 'acc':
-            t = "{} {} {}".format(acc, i_simbol, i_number)
-            print(t)
-            acc = eval(t)
-            i += 1
-        elif i_instruction == 'jmp':
-            t = "{} {} {}".format(i, i_simbol, i_number)
-            print(t)
-            i = eval(t)
-    else:
-        break
 
+# D8.1
+acc = count_acc(my_boot, access_flag)
 print('Acc at first repetition:', acc)
